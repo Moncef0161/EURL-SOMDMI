ea05e2f48c09a2c9da120ae5f6c92c0c51035788
RiadBensemmane <riad200339@gmail.com>
2025-08-08 18:58:33 +0100

    Initialize "Atelier" module with "maintenance" dep

---
+++ odoo/custom_addons/atelier/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    "name": "Atelier",
+    "author": "Riad Bensemmane",
+    "version": "18.0.0.1.0",
+    "depends": ["maintenance"],
+    "data": [
+        'views/maintenance_views.xml',
+    ]
+}
