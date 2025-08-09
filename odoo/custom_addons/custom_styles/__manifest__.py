e0094b226edf956ad7b7719236bc9f0d7661eda2
RiadBensemmane <riad200339@gmail.com>
2025-08-09 19:21:19 +0100

    Init Custom Styles module

---
+++ odoo/custom_addons/custom_styles/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Custom Styles",
+    "author": "Riad Bensemmane",
+    "version": "18.0.0.1.0",
+    "depends": ["web"],
+    'assets': {
+        # 'web._assets_primary_variables': [
+        #     'custom_styles/static/src/scss/primary_variables.scss',
+        # ],
+    },
+}
