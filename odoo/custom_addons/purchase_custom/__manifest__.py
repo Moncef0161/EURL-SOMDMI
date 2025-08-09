e87e4e2cd94b6df4e3a05fe61df00d631747ddbe
RiadBensemmane <riad200339@gmail.com>
2025-08-09 18:39:17 +0100

    Init Custom Purchase module

---
+++ odoo/custom_addons/purchase_custom/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    "name": "Purchase Custom",
+    "author": "Riad Bensemmane",
+    "version": "18.0.0.1.0",
+    "depends": ["purchase"],
+    "data": [
+        'views/purchase_views.xml',
+    ]
+}
