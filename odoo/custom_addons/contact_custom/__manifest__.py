fa7c21c5460dcc63788251bc70c4d027e210297b
RiadBensemmane <riad200339@gmail.com>
2025-08-10 13:02:25 +0100

    Init Contacts custom module

---
+++ odoo/custom_addons/contact_custom/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    "name": "Contacts Custom",
+    "author": "Riad Bensemmane",
+    "version": "18.0.0.1.0",
+    "depends": ["contacts"],
+    "data": [
+        'views/contacts_views.xml',
+    ]
+}
