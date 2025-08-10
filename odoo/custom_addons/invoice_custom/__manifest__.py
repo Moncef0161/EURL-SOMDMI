3e83a53421c732e3f7741f0c3b990674f82be05a
RiadBensemmane <riad200339@gmail.com>
2025-08-10 13:09:37 +0100

    Init Invoicing custom module

---
+++ odoo/custom_addons/invoice_custom/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    "name": "Invoice Custom",
+    "author": "Riad Bensemmane",
+    "version": "18.0.0.1.0",
+    "depends": ["account"],
+    "data": [
+        'views/account_views.xml',
+    ]
+}
