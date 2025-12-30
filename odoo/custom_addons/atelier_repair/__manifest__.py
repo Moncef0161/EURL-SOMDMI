5f35ce528c0281a625717b0d7f8b836ac19dac04
m.abdelaidoum <moncefabdelaidoum@gmail.com>
2025-12-30 13:42:28 +0100

    fix: add missing dependency 'somdmi_reports' in module manifest

--- odoo/custom_addons/atelier_repair/__manifest__.py
+++ odoo/custom_addons/atelier_repair/__manifest__.py
@@ -8,7 +8,7 @@
 Ce module renomme le module de Réparation en Atelier et personnalise ses menus.
 """,
     'author': 'Moncef',
-    'depends': ['repair', 'algerian_accounting', 'stock', 'contacts', 'purchase_repair'],
+    'depends': ['repair', 'algerian_accounting', 'stock', 'contacts', 'purchase_repair', 'somdmi_reports'],
     'data': [
         'data/product_category_data.xml',
         'data/stock_location_data.xml',
